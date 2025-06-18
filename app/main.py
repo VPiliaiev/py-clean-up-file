d9d2792fbdf736643cfb258b58b49d797fada9b8
Valerii <valerapiliaev@gmail.com>
2025-06-18 15:25:54 +0300

    add annotation for enter

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import os
 from types import TracebackType
 
@@ -6,13 +7,13 @@ class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self):
+    def __enter__(self) -> CleanUpFile:
         return self
 
     def __exit__(
             self,
             exc_type: type[BaseException] | None,
             exc_val: BaseException | None,
-            exc_tb: TracebackType | None,) -> None:
+            exc_tb: TracebackType | None, ) -> None:
         if os.path.exists(self.filename):
             os.remove(self.filename)
